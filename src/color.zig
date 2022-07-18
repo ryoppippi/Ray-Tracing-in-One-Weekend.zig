18a8b82572220ce8a4d3863edd0907863b829eaf
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-07-18 18:57:34 +0100

    8.3 Using Gamma Correction for Accurate Color Intensity

--- src/color.zig
+++ src/color.zig
@@ -1,15 +1,17 @@
 const std = @import("std");
+const math = std.math;
 const rtw = @import("rtweekend.zig");
 
 const SType = rtw.SType;
 const Color = rtw.Color;
+const sqrt = math.sqrt;
 
 pub fn writeColor(writer: anytype, pixel_color: Color, samples_per_pixel: anytype) !void {
     const T = ensureInt(@TypeOf(samples_per_pixel));
     const scale: SType = 1.0 / @intToFloat(SType, samples_per_pixel);
-    const r: SType = pixel_color[0] * scale;
-    const g: SType = pixel_color[1] * scale;
-    const b: SType = pixel_color[2] * scale;
+    const r: SType = sqrt(pixel_color[0] * scale);
+    const g: SType = sqrt(pixel_color[1] * scale);
+    const b: SType = sqrt(pixel_color[2] * scale);
 
     try writer.print("{d} {d} {d}\n", .{
         clp(r, T),
