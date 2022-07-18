fa9a27dbc7c253572ca0d965a927e47e74190a5a
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-07-18 19:59:15 +0100

    9.3 Modeling Light Scatter and Reflectance

--- src/material.zig
+++ src/material.zig
@@ -1,1 +1,29 @@
+const std = @import("std");
+const math = std.math;
+const rtw = @import("rtweekend.zig");
+const vec = @import("vec.zig");
+const ray = @import("ray.zig");
+const hittable = @import("hittable.zig");
+
+const Color = rtw.Color;
+const Ray = ray.Ray;
+const HitRecord = hittable.HitRecord;
+const RandGen = rtw.RandGen;
+
 // pub const material = struct {}
+
+pub const Lambertian = struct {
+    albedo: Color,
+    const Self = This();
+
+    pub fn init(albedo: Color) Self {
+        return Self{ .albedo = albedo };
+    }
+
+    pub fn scatter(self: Self, r_in: Ray, rec: *HitRecord, color: Color, scattered: Ray, rnd: *RandGen) bool {
+        const scatter_direction = rec.*.normal * vec.randomUnitVector(rnd, Color);
+        const scattered = Ray{ .origin = rec.*.p, .direction = scatter_direction };
+        attenuation = self.albedo;
+        return true;
+    }
+};
